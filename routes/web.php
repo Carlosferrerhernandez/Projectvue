66540492936894d7ed6981439823b60ee9aee19c
Carlos <carlosferrerhernandez10@gmail.com>
2018-09-10 20:01:52 -0500

    Updade changes
    - Routes auth log

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Ruta inicio*/
+Route::get('/', function () {
+    if (Auth::guest()) {
+        return view('welcome');  
+        //auth/login
+    }
+        return view('/');
+})->name('inicio');
